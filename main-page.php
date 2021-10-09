45cfcc04c8544177c8f7d02c25f313bcaea19ade
PAVILION\Павел <slap96ya@gmail.com>
2021-10-09 13:28:58 +0300

    php

--- main-page.php
+++ main-page.php
@@ -8,6 +8,22 @@ Template Name: Home Page
 <?php $projects_gallery = carbon_get_post_meta($page_id, 'projects'); ?>
 <?php get_header(); ?>
 <main class="main main-page">
+        /*
+        <?php
+        /* Start the Loop */
+        while ( have_posts() ) :
+            the_post();
+        ?>
+            <div class="entry-content">
+            <?php
+                the_content();
+            ?>
+            </div><!-- .entry-content -->
+
+        <?php endwhile; // End of the loop. ?>
+        */
+
+
     <section class="container-fluid slider-section">
         <div class="bg-fon"><img style="width:100%;height:100%" src="<?php bloginfo('template_url'); ?>/assets/img/slider.png" alt=""></div>
         <div class="slider main-slider">
