55c0ed4b983ef494628ab90f06b78f08f901e8d5
PAVILION\Павел <slap96ya@gmail.com>
2021-11-02 19:36:13 +0200

    upd

--- single-cpt-blank-template.php
+++ single-cpt-blank-template.php
@@ -6,21 +6,18 @@ Template Name: Blog Item
 
 <?php get_header(); ?>
 <main class="main">
-        
+    <div class="container">
+    <?php
+                if ( function_exists('yoast_breadcrumb') ) {
+                yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
+            }
+		?>  
     <?php
         if (have_posts()) {
             custom_post_types_get_custom_template();}
         }
     ?>
-    <section class="interesting-products">
-        <div class="container">
-            <?php
-                if ( function_exists('yoast_breadcrumb') ) {
-                yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
-            }
-		?>
-        </div>
-    </section>
-    
+    </div>
+
 </main>
 <?php get_footer(); ?>
