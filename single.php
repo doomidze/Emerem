e8ff1287807dc1455545411c0531fab880d789da
doomidze <sproject.st@gmail.com>
2021-11-20 12:42:44 +0200

    change product page with & fix header

--- single.php
+++ single.php
@@ -10,30 +10,29 @@
 
 <main class="main product">
 
-    <div class="container">
+    <div class="container-fluid">
 
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <?php
-            if ( function_exists('yoast_breadcrumb') ) {
-            yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
-            }
-            ?>
-            <?php
-                if (have_posts()) {
-                    while (have_posts()) {
-                        the_post(); ?>
-                        <section id="project-item" class="project-item container">
-                                <h1 class="project-item-title"><?php the_title()?></h1>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <?php the_content(); ?>
-                                    </div>
-                                </div>
-                            </section>
-                        <?php
-                    }
+                if ( function_exists('yoast_breadcrumb') ) {
+                    yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
                 }
             ?>
+            <h1 class="project-item-title"><?php the_title()?></h1>   
+
+            <section id="project-item" class="project-item">
+                <div class="row">
+                    <?php
+                        if (have_posts()) {
+                            while (have_posts()) {
+                                the_post(); ?>                        
+                                    <?php the_content(); ?>
+                                <?php
+                            }
+                        }
+                    ?>
+                </div>
+            </section>
 
         </article><!-- #post-<?php the_ID(); ?> -->
 
