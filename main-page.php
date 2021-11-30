ee8cc4cadfbd420bdf622b0af4c409b5beeb9c48
DESKTOP-N0QI4UB\Pashkve <slap96ya@gmail.com>
2021-12-01 00:30:01 +0200

    about-us restyle

--- main-page.php
+++ main-page.php
@@ -67,8 +67,8 @@ Template Name: Home Page
     
 
 
-    <section class="about-us-container">
-        <div class="about-us-wrapper">
+    <section class="about-us-container ">
+        <div class="about-us-wrapper container-fluid">
             <div class="pipe-img col-xs-12 col-sm-12 col-md-6 ">
                 <img src="<?php echo wp_get_attachment_image_url(carbon_get_post_meta($page_id, 'about_us_img'), 'full'); ?>" alt="" style="width:100%;height:auto">
             </div>
